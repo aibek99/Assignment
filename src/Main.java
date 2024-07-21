e0d2a00fa2fbbd241ee08e78462a53127d2ce314
aibek.bakirov <aibek.bakirov99@gmail.com>
2024-07-21 23:51:11 +0600

    updated the solution of assignment

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,68 @@
 import arraylist.MyArrayList;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        MyArrayList<Integer> ar = new MyArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            ar.add(scan.nextInt());
+        ArrayList<Integer> arList = new ArrayList<>();
+        Collections.addAll(arList, 15, 10, 12, 13, 17, 11, 9, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Vector<Integer> vList = new Vector<>(arList);
+
+        Stack<Integer> stList = new Stack<>();
+        stList.addAll(arList);
+
+        LinkedList<Integer> liList = new LinkedList<>(arList);
+
+        MyArrayList<Integer> myArList1 = new MyArrayList<>();
+        for (Integer i : arList) {
+            myArList1.add(i);
         }
-        ar.bubbleSort(true);
-        for (int i = 0; i < n; i++) {
-            System.out.println(ar.get(i));
+
+        MyArrayList<Integer> myArList2 = new MyArrayList<>();
+        myArList2.addAll(arList);
+
+        MyArrayList<Integer> myArList3 = new MyArrayList<>(arList);
+
+        System.out.print("ArrayList: ");
+        sortPrint(arList);
+
+        System.out.print("Vector: ");
+        sortPrint(vList);
+
+        System.out.print("Stack: ");
+        sortPrint(stList);
+
+        System.out.print("LinkedList: ");
+        sortPrint(liList);
+
+        myArList1.bubbleSort();
+        System.out.print("MyArrayList inserting with add method: ");
+        for (int i = 0; i < myArList1.size(); i++) {
+            System.out.print(myArList1.get(i) + " ");
         }
+        System.out.println();
+
+        myArList2.bubbleSort();
+        System.out.print("MyArrayList inserting with addAll method: ");
+        for (int i = 0; i < myArList2.size(); i++) {
+            System.out.print(myArList2.get(i) + " ");
+        }
+        System.out.println();
+
+        myArList3.bubbleSort();
+        System.out.print("MyArrayList inserting with MyArrayList constructor: ");
+        for (int i = 0; i < myArList2.size(); i++) {
+            System.out.print(myArList2.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static <E extends Comparable<E>> void sortPrint(List<E> ar) {
+        MyArrayList.bubbleSort(ar);
+        for (E i : ar) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
--- src/arraylist/MyArrayList.java
+++ src/arraylist/MyArrayList.java
@@ -1,35 +1,33 @@
 package arraylist;
 
 import java.util.Collection;
+import java.util.List;
 
 interface MyList<E extends Comparable<E>> {
     void add(E e);
     E get(int index);
     E remove(int index);
-    boolean addAll(Collection<? extends E> c);
+    void addAll(Collection<? extends E> c);
     int size();
 }
 
 public class MyArrayList<E extends Comparable<E>> implements MyList<E> {
-    private E[] array;
+    private Object[] array;
     private int size;
     private static final int defaultSize = 10;
 
-    @SuppressWarnings("unchecked")
     public MyArrayList() {
-        array = (E[]) new Comparable[defaultSize];
+        array = new Object[defaultSize];
         size = 0;
     }
 
-    @SuppressWarnings("unchecked")
     public MyArrayList(Collection<? extends E> c) {
-        super();
-        E[] a = (E[]) c.toArray();
+        this();
+        Object[] a = c.toArray();
         if (a.length == 0) return;
         int newCapacity = a.length;
-        assert this.array != null;
         if (newCapacity > this.array.length) {
-            E[] elements = (E[])new Comparable[newCapacity];
+            Object[] elements = new Comparable[newCapacity];
             System.arraycopy(this.array, 0, elements, 0, size);
             System.arraycopy(a, 0, elements, 0, a.length);
             this.array = elements;
@@ -43,49 +41,49 @@ public class MyArrayList<E extends Comparable<E>> implements MyList<E> {
         return capacity * 3 / 2 + 1;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public void add(E e) {
         if (size == array.length) {
             int newCapacity = newCapacity(array.length);
-            E[] a = (E[])new Comparable[newCapacity];
+            Object[] a = new Comparable[newCapacity];
             System.arraycopy(this.array, 0, a, 0, this.array.length);
             array = a;
         }
         array[size++] = e;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
         if (index >= array.length) {
-            throw new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException("MyArrayList index out of bounds");
         }
         return (E)array[index];
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E remove(int index) throws IndexOutOfBoundsException {
         if (index >= array.length) {
-            throw new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException("MyArrayList index out of bounds");
         }
-        E removed = array[index];
+        Object removed = array[index];
         size--;
         for (int i = index; i < size; i++) {
             array[i] = array[i + 1];
         }
-        return removed;
+        return (E) removed;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public boolean addAll(Collection<? extends E> c) {
-        E[] a = (E[]) c.toArray();
+    public void addAll(Collection<? extends E> c) {
+        Object[] a = c.toArray();
         if (a.length == 0) {
-            return false;
+            return;
         }
         int newCapacity = a.length + size;
         if (newCapacity > this.array.length) {
-            E[] elements = (E[])new Comparable[newCapacity];
+            Object[] elements = new Comparable[newCapacity];
             System.arraycopy(this.array, 0, elements, 0, size);
             System.arraycopy(a, 0, elements, size, a.length);
             this.array = elements;
@@ -93,24 +91,40 @@ public class MyArrayList<E extends Comparable<E>> implements MyList<E> {
             System.arraycopy(a, 0, this.array, size, a.length);
         }
         size = newCapacity;
-        return true;
     }
 
     public int size() {
         return size;
     }
 
-    public void bubbleSort(boolean flag) {
-        int check = 1;
-        if (!flag) check = -1;
+    public static <T extends Comparable<T>> void bubbleSort(List<T> list) {
+        for (int i = list.size() - 1; i > 0; i--) {
+            boolean isSorted = true;
+            for (int j = 0; j < i; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    isSorted = false;
+                    T tmp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, tmp);
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void bubbleSort() {
         for (int i = size - 1; i > 0; i--) {
             boolean isSorted = true;
             for (int j = 0; j < i; j++) {
-                if (check * array[j].compareTo(array[j + 1]) > 0) {
+                E el1 = (E) array[j];
+                E el2 = (E) array[j + 1];
+                if (el1.compareTo(el2) > 0) {
                     isSorted = false;
-                    E tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
+                    array[j] = el2;
+                    array[j + 1] = el1;
                 }
             }
             if (isSorted) {
